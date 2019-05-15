57bd93c6ff6473232c838908705ad690c943c607
hjw4595 <hjw4595@naver.com>
2019-05-16 00:27:29 +0900

    redux

---
+++ redux/src/App.js
@@ -0,0 +1,19 @@
+
+    import React, { Component, PropTypes } from 'react';
+    
+    const propTypes = { };
+
+    const defaultProps = { };
+
+    class App extends Component { 
+        render() {
+            return(
+                <div>App</div>
+            )
+        }
+    }
+
+    App.propTypes = propTypes;
+    App.defaultProps = defaultProps
+    
+    export default App;
---
+++ redux/src/Components/Counter.js
@@ -0,0 +1,23 @@
+
+    import React, { Component, PropTypes } from 'react';
+    
+        const propTypes = { };
+    
+        const defaultProps = { };
+
+        class $1 extends Component { 
+            constructor(props) { 
+                super(props);
+                
+            }
+            render() {
+                return(
+                    <div>$1</div>
+                )
+            }
+        }
+
+        $1.propTypes = propTypes;
+        $1.defaultProps = defaultProps
+        export default $1;
+    
