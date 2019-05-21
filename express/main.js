daa4bcb63fbbe5514d4ac2a177233a37fdfbdbf5
hjw4595 <hjw4595@naver.com>
2019-05-22 03:01:25 +0900

    express 사용

--- express/main.js
+++ express/main.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var app = express();
 var user = require('./routes/user');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
 
-app.get('/', function(req, res) {
-    res.send('Hello World');
-});
+// var myLogger = function(req, res, next) {
+//     console.log(req.url);
+//     next();
+// };
+
+// app.use(myLogger);
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use('/', express.static('public'));
 
 app.use('/user', user);
 
 app.listen(3000, function() {
-    console.log('Example App listening on port 3000');
+    console.log('Example App is listening on port 3000');
 });
--- express/routes/user.js
+++ express/routes/user.js
@@ -2,11 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/:id', function(req, res) {
-    res.send('Received a GET request, param:' + req.params.id);
+    res.send('Received a GET request, param: ' + req.params.id);
 });
 
 router.post('/', function(req, res) {
-    res.json({ success: true });
+    console.log(JSON.stringify(req.body, null, 2));
+
+    res.json({ 
+        success: true,
+        user: req.body.username
+    });
 });
 
 router.put('/', function(req, res) {
